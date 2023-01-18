0bf93d1b5a5b9268a39f54dd3495f7d9d220a53f
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2023-01-18 00:55:27 -0500

    Update the functionalities to count up and display it

--- app/src/main/java/com/example/kotlinpractice02counter/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice02counter/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.kotlinpractice02counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize variables
+        val welcomeText : TextView = findViewById(R.id.welcome_text)
+        val btn: Button = findViewById(R.id.btn)
+        // Add functionalities
+        btn.setOnClickListener{ increaseCounter() }
+    }
+
+    // Create a function
+    private fun increaseCounter() {
+        val counterText : TextView = findViewById(R.id.counter_text)
+        var counter = counterText.text.toString().toInt()
+        counter++
+        counterText.text = counter.toString()
     }
 }
